e2fae2f888f09a3b59aff19278d5693d9c6ffd0f
Sergey Lebedev <londeren@gmail.com>
2016-01-13 15:55:10 +0700

    update babel conf

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     loaders: [
       {
       test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
+      loaders: ['react-hot', 'babel-loader'],
       include: path.join(__dirname, 'src')
     },
       {
