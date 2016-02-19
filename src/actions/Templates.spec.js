f3a826d7b1664d26ec9c93d4febf1409ced9203c
Sergey Lebedev <londeren@gmail.com>
2016-02-19 17:36:23 +0700

    test for Template actions

---
+++ src/actions/Templates.spec.js
@@ -0,0 +1,28 @@
+import {expect} from 'chai';
+import {addTemplate} from './Templates';
+import {TEMPLATE_ADD} from '../constants/ActionTypes';
+
+
+describe('templates actions', () => {
+
+  describe('addTemplate', () => {
+
+    it('should create an action to create a template', () => {
+      const name = 'Template name';
+      const items = [
+        {
+          "name": "first item"
+        }
+      ];
+      const expectedAction = {
+        type: TEMPLATE_ADD,
+        name,
+        items
+      };
+
+      expect(addTemplate(name, items)).to.include(expectedAction);
+    });
+
+  });
+
+});
