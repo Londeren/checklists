8416c7fea652ac74e4098aefb06b11a981639da1
Sergey Lebedev <londeren@gmail.com>
2016-03-23 18:08:55 +0700

    Templates reducer tests
    init

---
+++ src/reducers/Templates.spec.js
@@ -0,0 +1,16 @@
+import chai, {expect} from 'chai';
+import spies from 'chai-spies';
+
+chai.use(spies);
+
+
+describe('Templates reducer', () => {
+  describe('TEMPLATE_ADD', () => {
+    it('should return the initial state');
+
+  });
+
+  describe('TEMPLATE_UPDATE', () => {
+    it('should return the initial state');
+  });
+});
