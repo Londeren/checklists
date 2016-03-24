9c436163d716c999e80f202483827b04d913d035
Sergey Lebedev <londeren@gmail.com>
2016-03-24 19:39:26 +0700

    Templates reducer tests
    test for TEMPLATE_ADD

--- src/reducers/Templates.spec.js
+++ src/reducers/Templates.spec.js
@@ -1,16 +1,53 @@
 import chai, {expect} from 'chai';
 import spies from 'chai-spies';
+import reducer from './Templates';
+import {TEMPLATE_ADD, TEMPLATE_UPDATE} from '../constants/ActionTypes';
 
 chai.use(spies);
 
 
 describe('Templates reducer', () => {
-  describe('TEMPLATE_ADD', () => {
-    it('should return the initial state');
-
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).to.be.eql([]);
   });
 
-  describe('TEMPLATE_UPDATE', () => {
-    it('should return the initial state');
+  it('should handle TEMPLATE_ADD', () => {
+    expect(reducer([], {
+      type: TEMPLATE_ADD,
+      id: '1',
+      name: 'test',
+      items: []
+    })).to.be.eql([
+      {
+        id: '1',
+        name: 'test',
+        items: []
+      }
+    ]);
+
+    expect(reducer([{
+      id: '1',
+      name: 'test',
+      items: []
+    }], {
+      type: TEMPLATE_ADD,
+      id: '2',
+      name: 'test2',
+      items: [{name: 'first item'}]
+    })).to.be.eql([
+      {
+        id: '1',
+        name: 'test',
+        items: []
+      },
+      {
+        id: '2',
+        name: 'test2',
+        items: [{name: 'first item'}]
+      }
+    ]);
   });
+
+  it('should handle TEMPLATE_UPDATE');
+
 });
