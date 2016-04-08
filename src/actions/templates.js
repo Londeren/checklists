01c0ee5d7bce1f390027e05f272ce193a3c58500
Sergey Lebedev <londeren@gmail.com>
2016-04-08 17:59:53 +0700

    fetch templates from server

--- src/actions/templates.js
+++ src/actions/templates.js
@@ -1,5 +1,10 @@
 import {v4 as uniqueId}  from 'node-uuid';
-import {TEMPLATE_ADD, TEMPLATE_UPDATE} from '../constants/ActionTypes';
+import fetch from 'isomorphic-fetch';
+import {
+  TEMPLATE_ADD,
+  TEMPLATE_UPDATE,
+  TEMPLATE_FETCH_STARTED, TEMPLATE_FETCH_COMPLETED, TEMPLATE_FETCH_ERROR
+} from '../constants/ActionTypes';
 
 
 export function addTemplate(name, items) {
@@ -17,5 +22,39 @@ export function updateTemplate(id, name, items) {
     id,
     name,
     items
+  }
+}
+
+
+function requestTemplates() {
+  return {
+    type: TEMPLATE_FETCH_STARTED
+  }
+}
+
+function receiveTemplates(json) { console.log(json);
+  return {
+    type: TEMPLATE_FETCH_COMPLETED,
+    templates: json
+  }
+}
+
+
+function errorTemplates(error) {
+  return {
+    type: TEMPLATE_FETCH_ERROR,
+    error: error
+  }
+}
+export function fetchTemplates() {
+  return dispatch => {
+    dispatch(requestTemplates());
+
+    return fetch('/templates.json')
+      .then(response => response.json())
+      .then(json =>
+        dispatch(receiveTemplates(json.templates))
+      )
+      .catch(error => dispatch(errorTemplates(error)));
   }
 }
