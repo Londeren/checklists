95b831f6635a7c53c775af1dbe68f85544bb1244
Sergey Lebedev <londeren@gmail.com>
2016-04-11 16:47:00 +0700

    misc

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import { routeReducer } from 'react-router-redux';
-import { DOCUMENT_FETCH_COMPLETED } from '../actions/getDocumentsActions';
 import topMenu from '../reducers/TopMenu';
 import templates from '../reducers/Templates';
 import lists from '../reducers/Lists';
