c1ed70152b5cb0f93e56630b53325c9f17240405
Sergey Lebedev <londeren@gmail.com>
2016-04-20 11:58:36 +0700

    refactoring

--- src/components/AddItemButton.js
+++ src/components/AddItemButton.js
@@ -1,20 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 
 
-export default class AddItemButton extends Component {
-  render() {
-    var {onClickAction, buttonAdditionalClasses} = this.props;
-
-    return (
-        <div className="add-item">
-          <button type="button" className={'btn btn-info-outline btn-block ' + buttonAdditionalClasses } onClick={onClickAction}>{this.props.children}</button>
-        </div>
-    );
-  }
+export default function AddItemButton({ onClickAction, buttonAdditionalClasses, children }) {
+  return (
+    <div className="add-item">
+      <button type="button" className={'btn btn-info-outline btn-block ' + buttonAdditionalClasses } onClick={onClickAction}>{children}</button>
+    </div>
+  );
 }
 
 AddItemButton.propTypes = {
   onClickAction: PropTypes.func.isRequired,
-  children: PropTypes.string.isRequired,
-  buttonAdditionalClasses: PropTypes.string
+  buttonAdditionalClasses: PropTypes.string,
+  children: PropTypes.string.isRequired
 };
