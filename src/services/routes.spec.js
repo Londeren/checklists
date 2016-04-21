45d693af72fd5d5d0c4d2a6167d3ce7001c0e7b0
Sergey Lebedev <londeren@gmail.com>
2016-04-21 12:08:24 +0700

    tests for service routes

--- src/services/routes.spec.js
+++ src/services/routes.spec.js
@@ -15,5 +15,18 @@ describe('Routes service', () => {
       expect(getRouteUrl(template, params)).to.equal(expected);
     });
 
+    it('should replace params if they exists', () => {
+      const template = '/path/to/:any/params';
+      const params = {
+        params: 'params'
+      };
+
+      const expected = '/path/to/:any/params';
+
+      expect(getRouteUrl(template, params)).to.equal(expected);
+    });
+
+
+
   });
 });
