f43486bfe253b30048ea4ac92a359195396b119a
Sergey Lebedev <londeren@gmail.com>
2016-04-21 12:00:48 +0700

    tests for Lists reducer

---
+++ src/reducers/Lists.spec.js
@@ -0,0 +1,81 @@
+import chai, {expect} from 'chai';
+import spies from 'chai-spies';
+import reducer from './Lists';
+import {LIST_ADD, LIST_UPDATE} from '../constants/ActionTypes';
+
+describe('Templates reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).to.be.eql([]);
+  });
+
+  it('should handle LIST_ADD', () => {
+    expect(reducer([], {
+      type: LIST_ADD,
+      id: '1',
+      templateId: '0.1',
+      name: 'test',
+      items: []
+    })).to.be.eql([
+      {
+        id: '1',
+        templateId: '0.1',
+        name: 'test',
+        items: []
+      }
+    ]);
+
+    expect(reducer([{
+      id: '1',
+      templateId: '0.1',
+      name: 'test',
+      items: []
+    }], {
+      type: LIST_ADD,
+      id: '2',
+      templateId: '0.2',
+      name: 'test2',
+      items: [{name: 'first item'}]
+    })).to.be.eql([
+      {
+        id: '1',
+        templateId: '0.1',
+        name: 'test',
+        items: []
+      },
+      {
+        id: '2',
+        templateId: '0.2',
+        name: 'test2',
+        items: [{name: 'first item'}]
+      }
+    ]);
+  });
+
+  it('should handle LIST_UPDATE', () => {
+    expect(reducer([], {
+      type: LIST_UPDATE,
+      id: '1',
+      name: 'test',
+      items: []
+    })).to.be.eql([]);
+
+    expect(reducer([{
+      id: '1',
+      templateId: '0.1',
+      name: 'test',
+      items: []
+    }], {
+      type: LIST_UPDATE,
+      id: '1',
+      name: 'test1',
+      items: [{name: 'first item'}]
+    })).to.be.eql([
+      {
+        id: '1',
+        templateId: '0.1',
+        name: 'test1',
+        items: [{name: 'first item'}]
+      }
+    ]);
+  });
+});
