aab72f80012d1e073295db62f642e67463bc5429
Sergey Lebedev <londeren@gmail.com>
2016-05-26 19:37:03 +0300

    create logger

---
+++ backend/libs/log.js
@@ -0,0 +1,16 @@
+import winston from 'winston';
+import config from '../../config';
+
+export default function(module) {
+  const path = module.filename.split('/').slice(-2).join('/');
+
+  return new winston.Logger({
+    transports: [
+      new winston.transports.Console({
+        colorize: true,
+        level: config.get('NODE_ENV') === 'development' ? 'debug' : 'error',
+        label: path
+      })
+    ]
+  });
+}
