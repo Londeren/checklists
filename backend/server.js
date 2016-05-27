9930d4889559fd92057f345e69432a3c1acb583d
Sergey Lebedev <londeren@gmail.com>
2016-05-27 11:06:27 +0300

    logger, node_env=dev

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import Koa from 'koa';
 import router from './router';
 import config from '../config';
+import logger from './libs/log';
 
+const log = logger(module);
 const app = new Koa();
 
 app.use(router.routes());
 
 app.listen(config.get('backend').port, function() {
-  console.log(`Start listening http://localhost:${config.get('backend').port}/`);
+  log.info(`Start listening http://localhost:${config.get('backend').port}/`);
 });
