101066b3462cf422798917ab94f3e8dd9d98946d
Sergey Lebedev <londeren@gmail.com>
2016-06-01 18:56:47 +0300

    mongoose models

---
+++ backend/Models/Item.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import {v4 as uniqueId}  from 'node-uuid';
+
+const itemSchema = mongoose.Schema({
+  id: {type: String, default: uniqueId, required: true},
+  name: {type: String},
+  done: {type: Boolean}
+});
+
+const Item = mongoose.Model('Item', itemSchema);
+
+export default Item;
---
+++ backend/Models/Template.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import {v4 as uniqueId}  from 'node-uuid';
+import Item from './Item';
+
+const templateSchema = mongoose.Schema({
+  id: {type: String, default: uniqueId, required: true},
+  name: {type: String},
+  items: [Item.schema]
+});
+
+const Template = mongoose.Model('Template', templateSchema);
+
+export default Template;
