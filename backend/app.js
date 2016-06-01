f9cb5c7f2e5f75655e5c8a35c85bc51f214a550c
Sergey Lebedev <londeren@gmail.com>
2016-06-01 18:56:35 +0300

    commect to mongo with mongoose

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,18 @@ import Koa from 'koa';
 import router from './router';
 import config from '../config';
 import logger from './libs/logger';
+import db from './libs/db';
 
 const log = logger(module);
 const app = new Koa();
 
 app.use(router.routes());
 
-app.listen(config.get('backend').port, function() {
-  log.info(`Start listening http://localhost:${config.get('backend').port}/`);
+db.once('open', () => {
+  app.listen(config.get('backend:port'), function() {
+    log.info(`Start listening http://localhost:${config.get('backend:port')}/`);
+  });
 });
-
 
 
 export default app;
---
+++ backend/libs/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import config from '../../config';
+import logger from '../libs/logger';
+
+const log = logger(module);
+
+mongoose.connect(config.get('backend:mongoose:uri'));
+
+const db = mongoose.connection;
+
+db.on('error', log.error);
+
+export default db;
