49f9b7b3ace6763af231a68da8e69d232668c71a
Sergey Lebedev <londeren@gmail.com>
2016-07-21 19:20:12 +0300

    add config for testing

--- config/index.js
+++ config/index.js
@@ -1,8 +1,11 @@
 var nconf = require('nconf');
 var path = require('path');
 
-nconf.argv()
+nconf = nconf.argv()
   .env()
   .file({file: path.join(__dirname, '/config.json')});
+
+if(nconf.get('NODE_ENV') === 'test')
+  nconf.file({file: path.join(__dirname, '/config.test.json')})
 
 module.exports = nconf;
