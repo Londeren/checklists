8b286806852bbe56755ee40c10a6ea8e0a3b07ab
Sergey Lebedev <londeren@gmail.com>
2016-07-27 17:10:26 +0300

    bugfix

--- backend/http/middlewares/errorHandler.js
+++ backend/http/middlewares/errorHandler.js
@@ -3,7 +3,7 @@ export default async (ctx, next) => {
     await next();
   } catch(err) {
 
-    err.status = err.status || err.statusCode || 500;
+    ctx.status = err.status || err.statusCode || 500;
 
     ctx.body = {
       message: err.message
