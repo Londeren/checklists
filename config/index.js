b060aa0edc1f82c16ab337265d876530606aa46b
Sergey Lebedev <londeren@gmail.com>
2016-07-28 14:41:52 +0300

    change config files hierarchy

--- config/index.js
+++ config/index.js
@@ -1,11 +1,12 @@
 var nconf = require('nconf');
 var path = require('path');
 
-nconf = nconf.argv()
-  .env()
-  .file({file: path.join(__dirname, '/config.json')});
+nconf = nconf.argv().env();
 
-if(nconf.get('NODE_ENV') === 'test')
-  nconf.file({file: path.join(__dirname, '/config.test.json')})
+if (nconf.get('NODE_ENV') === 'test') {
+  nconf.file('test', path.join(__dirname, '/config.test.json'));
+}
+
+nconf.file('common', path.join(__dirname, '/config.json'));
 
 module.exports = nconf;
