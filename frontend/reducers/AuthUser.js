5f3d930455463d92626fa08a8969212284484baa
Sergey Lebedev <londeren@gmail.com>
2016-08-17 17:49:40 +0300

    add reducer for AuthUser

---
+++ frontend/reducers/AuthUser.js
@@ -0,0 +1,9 @@
+export default function(state = {
+  "isFetching": false,
+  "isAuthorized": false
+}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
--- frontend/reducers/index.js
+++ frontend/reducers/index.js
@@ -3,12 +3,14 @@ import { routeReducer } from 'react-router-redux';
 import topMenu from '../reducers/TopMenu';
 import templates from '../reducers/Templates';
 import lists from '../reducers/Lists';
+import authUser from '../reducers/AuthUser';
 
 const rootReducer = combineReducers({
   routing: routeReducer,
   topMenu,
   templates,
-  lists
+  lists,
+  authUser
 });
 
 export default rootReducer;
