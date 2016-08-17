d17c527dcdce884a3ac2da1f15bcf4ac080f1a45
Sergey Lebedev <londeren@gmail.com>
2016-08-17 17:48:24 +0300

    actions for auth user

---
+++ frontend/actions/AuthUser.js
@@ -0,0 +1,42 @@
+'use strict';
+import config from '../../config/config.json';
+import fetch from 'isomorphic-fetch';
+
+import { LOGIN_STARTED, LOGIN_COMPLETED, LOGIN_ERROR } from '../constants/ActionTypes';
+
+export function login(creds) {
+  return dispatch => {
+    dispatch({
+      type: LOGIN_STARTED,
+      isFetching: true,
+      isAuthorized: false,
+      creds
+    });
+
+    return fetch(`${config.base_path}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(creds)
+    })
+      .then(response => response.json())
+      .then(user =>
+        dispatch({
+            type: LOGIN_COMPLETED,
+            isFetching: false,
+            isAuthorized: true,
+            login: user.login
+          }
+        )
+      )
+      .catch(error => dispatch({
+          type: LOGIN_ERROR,
+          isFetching: false,
+          isAuthorized: false,
+          error
+        }
+      ));
+  }
+}
