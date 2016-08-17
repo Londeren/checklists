61938b4fc7c06428df6c5476bfde364c58e0930f
Sergey Lebedev <londeren@gmail.com>
2016-08-17 17:47:38 +0300

    create stub for Auth.login

--- backend/http/controllers/Auth.js
+++ backend/http/controllers/Auth.js
@@ -1,17 +1,14 @@
 'use strict';
+import {sign} from '../../libs/jwt';
 import HttpError from '../errors/HttpError';
 
 export default class Auth {
   static async login(ctx) {
     ctx.body = {
-      login: 'test',
-      token: 'qwerty'
-    };
-  }
-
-  static async logout(ctx) {
-    ctx.body = {
-      status: 'success'
+      token: sign({
+        id: '1',
+        login: 'admin'
+      })
     };
   }
 }
