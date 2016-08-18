62e7aa6b7d8ca3ab06fd4b50e70c08f32d96cd7d
Sergey Lebedev <londeren@gmail.com>
2016-08-18 16:37:30 +0300

    fix tests
    add authorization

--- backend/http/controllers/Templates.spec.js
+++ backend/http/controllers/Templates.spec.js
@@ -1,21 +1,34 @@
 import {expect, should} from 'chai';
 import supertest from 'supertest-as-promised';
+import find from 'lodash/find';
 import {connect, disconnect, mongoose} from '../../libs/db';
 import seed from '../../seed';
 import seedTemplates from '../../seed/templates.json';
+import seedUsers from '../../seed/users.json';
 import app from '../../app';
+import User from '../../models/User';
 
 const api = supertest.agent(app.listen());
 
 
 describe('Templates controller', () => {
-  before(async () => await connect());
-  beforeEach(async () => await seed(mongoose.connection));
+  let accessToken;
+
+  before(async() => await connect());
+  beforeEach(async() => {
+    await seed(mongoose.connection);
+
+    const seedUser = find(seedUsers, {login: 'admin'});
+    const user = await User.findOne({login: seedUser.login});
+
+    accessToken = user.generateToken();
+  });
   after(() => disconnect());
 
 
-  it('should return all templates on call GET /api/templates', async () => {
+  it('should return all templates on call GET /api/templates', async() => {
     const response = await api.get('/api/templates')
+      .set('Authorization', `Bearer ${accessToken}`)
       .expect(200)
       .expect('Content-Type', /json/);
 
@@ -28,32 +41,37 @@ describe('Templates controller', () => {
     });
   });
 
-  it('should not save template with no passed parameters POST /api/templates', async () => {
+  it('should not save template with no passed parameters POST /api/templates', async() => {
     const response = await api.post('/api/templates')
+      .set('Authorization', `Bearer ${accessToken}`)
       .expect(400)
       .expect('Content-Type', /json/);
 
-    expect(response.body).have.property('message');
-    expect(response.body.message).have.property('message');
-    expect(response.body.message).have.property('errors');
+    expect(response.body).have.property('error');
+    expect(response.body.error).have.property('message');
+    expect(response.body.error.message).have.property('message');
+    expect(response.body.error.message).have.property('errors');
   });
 
   it('should save template with proper parameters POST /api/templates', async() => {
     const newTemplate = {
       id: 'testId',
       name: 'test',
-      items: [{
-        id: 'testId1',
-        name: 'test1',
-        done: false
-      },{
-        id: 'testId2',
-        name: 'test2',
-        done: true
-      }]
+      items: [
+        {
+          id: 'testId1',
+          name: 'test1',
+          done: false
+        }, {
+          id: 'testId2',
+          name: 'test2',
+          done: true
+        }
+      ]
     };
 
     const response = await api.post('/api/templates')
+      .set('Authorization', `Bearer ${accessToken}`)
       .send(newTemplate)
       .expect(200)
       .expect('Content-Type', /json/);
@@ -61,14 +79,16 @@ describe('Templates controller', () => {
     expect(response.body).to.deep.equal(newTemplate);
   });
 
-  it('should not update template with no passed parameters PUT /api/templates', async () => {
+  it('should not update template with no passed parameters PUT /api/templates', async() => {
     const response = await api.put('/api/templates')
+      .set('Authorization', `Bearer ${accessToken}`)
       .expect(400)
       .expect('Content-Type', /json/);
 
-    expect(response.body).have.property('message');
-    expect(response.body.message).have.property('message');
-    expect(response.body.message).have.property('errors');
+    expect(response.body).have.property('error');
+    expect(response.body.error).have.property('message');
+    expect(response.body.error.message).have.property('message');
+    expect(response.body.error.message).have.property('errors');
   });
 
   it('should update template with proper parameters PUT /api/templates', async() => {
@@ -76,6 +96,7 @@ describe('Templates controller', () => {
     updateTemplate.name = 'New template name';
 
     const response = await api.post('/api/templates')
+      .set('Authorization', `Bearer ${accessToken}`)
       .send(updateTemplate)
       .expect(200)
       .expect('Content-Type', /json/);
