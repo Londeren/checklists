415a53d09af63f0da7e3c2b14387a42826d68d02
Sergey Lebedev <londeren@gmail.com>
2016-08-18 15:33:47 +0300

    add function verify

--- backend/libs/jwt.js
+++ backend/libs/jwt.js
@@ -1,9 +1,15 @@
 'use strict';
 import jwt from 'jsonwebtoken';
+import Promise from 'bluebird';
 import config from '../../config';
 
+Promise.promisifyAll(jwt);
 
 export const sign = (payload) => {
   return jwt.sign({...payload, iat: Math.floor(Date.now() / 1000)}, config.get('backend:jwt:secret'), {expiresIn: '1h'})
+};
+
+export const verify = (token) => {
+  return jwt.verifyAsync(token, config.get('backend:jwt:secret'));
 };
 
