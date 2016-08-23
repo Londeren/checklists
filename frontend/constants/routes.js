fa4ea9311a4da2df5bb139afee007da756746f15
Sergey Lebedev <londeren@gmail.com>
2016-08-23 18:10:13 +0300

    add index route

--- frontend/constants/routes.js
+++ frontend/constants/routes.js
@@ -1,3 +1,4 @@
+export const ROUTE_INDEX = '/';
 export const ROUTE_TEMPLATES_TEMPLATES = '/templates/';
 export const ROUTE_TEMPLATES_CREATE = '/templates/create';
 export const ROUTE_TEMPLATES_VIEW_TEMPLATE = '/templates/view/:templateId';
